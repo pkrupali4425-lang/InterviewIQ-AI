4ac23d3a2de54d0f31de544e14f2fba5e115dd4f
pkrupali4425-lang <pkrupali4425@gmail.com>
2026-08-05 23:31:20 +0530

    Updated Gemini model

--- backend/rag.py
+++ backend/rag.py
@@ -55,8 +55,8 @@ Question:
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt,
-    )
+    model="gemini-2.5-flash-lite",
+    contents=prompt,
+)
 
     return response.text
